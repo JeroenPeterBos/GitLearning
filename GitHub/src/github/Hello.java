e63fcd545bf023a357fb132f13d27292948620a0
Laurens <Laurensvdhoef@hotmail.com>
2015-11-27 14:34:17 +0100

    call me maybe

--- GitHub/src/github/Hello.java
+++ GitHub/src/github/Hello.java
@@ -3,7 +3,7 @@ package github;
 public class Hello {
 
 	public static void main(String[] args){
-		System.out.println("interesting!");
+		System.out.println("inDaClub");
 	}
 	
 	public void hit(){
